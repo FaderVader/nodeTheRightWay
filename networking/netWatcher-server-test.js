f3ced7b80633a282a72c4c04a6dd0298c075e2fa
FaderVader <jakobviggohansen@gmail.com>
2018-06-10 11:59:54 +0200

    progress

---
+++ networking/netWatcher-server-test.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const server = require('net').createServer(connection => {
+    console.log('Client connected');
+
+
+    const firstChunk = '{"type": "changed", "timesta"';
+    const secondChunk = 'mp": 1450694370094}\n';
+
+    connection.write(firstChunk);
+
+    const timer = setTimeout(() => {
+        connection.write(secondChunk);
+        connection.end;
+    }, 100);
+
+    connection.on('end', () => {
+        console.log('Client disconnected');
+    });
+});
+
+server.listen(60300, function() {
+    console.log('Test server is listening for you!');
+});
