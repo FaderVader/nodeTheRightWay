7183a1a0262580ff4eb394f78f0100370fa9f1ed
FaderVader <jakobviggohansen@gmail.com>
2018-07-03 12:49:34 +0200

    changes to client

--- networking/netWatcher-client.js
+++ networking/netWatcher-client.js
@@ -1,6 +1,13 @@
 'use strict';
+const serverAddres = process.argv[2];
+
+if (!serverAddres) {
+    console.log('Error: no server address specified');
+    process.exit();
+}
+
 const net = require('net');
-const client = net.connect({ port: 60300 });
+const client = net.connect({ port: 60300, host: serverAddres });  //'192.168.1.199'
 
 client.on('data', data => {
     const message = JSON.parse(data);
@@ -13,4 +20,8 @@ client.on('data', data => {
     } else {
         console.log(`Unrecognized message type: ${message.type}`);
     }
+})
+
+client.on('error', (error) => {
+    console.log(`Error while connecting to: ${serverAddres}`);
 })
