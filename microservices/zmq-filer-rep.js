d1b3402537eaa04d572075408df233deed5ffb2e
FaderVader <jakobviggohansen@gmail.com>
2018-07-05 16:19:11 +0200

    errorhandling with promises

--- microservices/zmq-filer-rep.js
+++ microservices/zmq-filer-rep.js
@@ -7,7 +7,13 @@ const responder = zmq.socket('rep');
 responder.on('message', data => {
     readAndReturn(data).then(response => {
         responder.send(response);
-    });
+    })
+    .catch(err => {
+        console.log(`Got error: ${err.message}`)
+        responder.send(JSON.stringify({
+            content: 'Error while accessing file'
+        }));
+    })
 });
 
 responder.bind('tcp://*:60401', err => {
@@ -20,19 +26,28 @@ process.on('SIGINT', () => {
 });
 
 
-function readAndReturn(data) {
+ function readAndReturn(data) {
     return new Promise((resolve, reject) => {
-        const request = JSON.parse(data);
-        console.log(`got request for: ${request.path}`)
+            const request = JSON.parse(data);
+            console.log(`got request for: ${request.path}`)
 
-        fs.readFile(request.path, (err, content) => {
+            readFile(request.path)
+            .then(response => { resolve(response)})
+            .catch(err => { reject(err)})
+    });
+}
+
+function readFile(path) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(path, (err, content) => {
+            if (err) { return reject(err)}
+
             let response = JSON.stringify({
                 content: content.toString(),
                 timestamp: Date.now(),
                 pid: process.pid
-            })
-            console.log(`sending response: ${response}`);
-            resolve(response);
-        });
-    });
+            });
+            resolve(response)
+        })
+    })
 }
--- microservices/zmq-filer-req.js
+++ microservices/zmq-filer-req.js
@@ -1,23 +1,21 @@
 'use strict';
 
 const hostAddress = process.argv[2];
-if (!hostAddress) {
-    console.log('Error - no host specified');
+const fileName = process.argv[3];
+if (!(hostAddress && fileName)) {
+    console.log('Error - no host or target-file specified ');
     process.exit();
 }
 
 const zmq = require('zeromq');
-const fileName = process.argv[3];
-
 const requester = zmq.socket('req');
 
-requester.on('message', data => {
-    // console.log(`Raw data: ${data}`);
+requester.on('message', data => {    
     const response = JSON.parse(data);
     console.log(`Recieved response: ${response.content}`);
 });
 
-requester.connect(`tcp://${hostAddress}:60401`); //'tcp://localhost:60401'
+requester.connect(`tcp://${hostAddress}:60401`);
 
 console.log(`Sending a request for: ${fileName}`);
 requester.send(JSON.stringify({ path: fileName}));
